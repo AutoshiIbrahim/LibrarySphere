07c1c31b5342c1292af94a7135b455a94005269a
Autoshi Ibrahim <98845573+AutoshiIbrahim@users.noreply.github.com>
2024-07-16 13:32:58 +0600

    User master

---
+++ User.Master.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LibrarySphere.UserScreen
+{
+    public partial class User : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["role"]!=null  && Session["role"].ToString()=="user")
+            {
+                if(!IsPostBack)
+                {
+                    lblUserName.Text = Session["username"].ToString();
+                }
+            }
+            else
+            {
+                Response.Redirect("signout.aspx");
+            }
+          
+        }
+    }
+}
